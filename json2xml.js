06e80cfb89f2ff0b8e040addd0be79d91e32f109
tdurivaux <tdurivaux@users.noreply.github.com>
2018-05-22 15:33:30 +0200

    Standard update, arguments, and 17.2
    Standard update implemented: elements with maxOccurs <= 1 in the XSD are no longer arrays in JSON.
    
    Input and output files are no longer hard-coded, but given as arguments.
    
    Sequences reflect version 17.2 (was 16.2).

--- json2xml.js
+++ json2xml.js
@@ -1,80 +1,114 @@
 #!/usr/bin/node
 
 var fs = require('fs');
-var input = {};
-var output = "";
-var sequences = {};
 
-fs.readFile("./sequences/sequences-162.json", "utf8", function (err, data) {
+var json = {};
+var xml = '';
+var sequences = {};
+var debug = false; // display debug messages for sequences
+
+var args = process.argv.slice(2);
+
+// read sequences file
+fs.readFile('./sequences/sequences-172.json', 'utf8', function (err, data) {
 	if (err) {
-		console.log("error reading sequences.json");
+		console.log('error reading sequences.json');
 	}
 	else {
 		sequences = JSON.parse(data);
 	}
 });
 
-fs.readFile('./input.json', 'utf8', function (err, data) {
-	if (err) throw err;
-	input = JSON.parse(data);
+// read input (JSON file)
+fs.readFile(args[0], 'utf8', function (err, data) {
+	if (err) {
+		console.error('Invalid input file');
+		return -1
+	}
+	try {
+		json = JSON.parse(data);
+	} catch(err) {
+		console.error('Invalid JSON input');
+		return
+	}
 	// only one key/value pair in element (root)
-	key = Object.keys(input)[0];
-	parse(input[key], key);
-	fs.writeFile("./output.xml", output, function(err) {
+	key = Object.keys(json)[0];
+	parse(json[key], key, '');
+	fs.writeFile(args[1], xml, function(err) {
 		if(err) {
-			return console.log(err + "error writing to output file");
+			return console.log(err + 'error writing to output file');
 		}
 	});
-	// console.log("\n\n" + output);
 });
 
-function parse(obj, key) {
-	var xpath;
+
+function parse(obj, key, xpath) {
 	// loop over array values
-	for (var el of obj) {
-		// opening el
-		output += "<" + key;
-		// attributes
-		if ("$" in el) {
-			for (var attr in el.$) {
-				output += " " + attr + "=\"" + el.$[attr] + "\"";
-			}
+	if (Array.isArray(obj)) {
+		for (var el of obj) {
+			sub_parse(key, el, xpath+key+'/');
 		}
-		output += ">";
-		// value
-		if ("_" in el) {
-			output += el._;
-		}
-		else { // children
-			var unorderedKeys = Object.keys(el);
-			// SORTING KEYS
-			var keys = [];
-			var sequence = [];
-			try {
-				sequence = sequences[xpath];
-				for (var e of sequence) {
-					if (unorderedKeys.indexOf(e) > -1) {
-						keys.push(e);
-					}
-				}
-				if (unorderedKeys.length != keys.length) { // missing keys in sequence
-					console.log("Sequence incomplete for " + xpath);
-					keys = unorderedKeys; // keep all keys, still unordered
-				}
-			}
-			catch (err) { // no sequence for xpath
-				console.log("No sequence found for " + xpath);
-				keys = unorderedKeys;
-			}
-			// KEYS SORTED
-			for (var k of keys) {
-				// do not process attributes and value
-				if (k != "$" && k != "_") {
-					parse(el[k], k, xpath+k+"/");
-				}
-			}
-		}
-		// closing element
-		output += "</" + key + ">";
+	} else {
+		sub_parse(key, obj, xpath+key+'/');
 	}
+}
+
+
+function sub_parse(key, el, xpath) {
+	// opening el
+	for (var i=0 ; i < (xpath.match(/\//g) || []).length -1 ; i++) {
+		xml += '\t';
+	}
+	xml += '<' + key;
+	// attributes
+	if ('$' in el) {
+		for (var attr in el.$) {
+			xml += ' ' + attr + '="' + el.$[attr] + '"';
+		}
+	}
+	xml += '>';
+	// value
+	if ('_' in el) {
+		xml += el._;
+	}
+	else { // children
+		xml += '\n';
+		var unorderedKeys = Object.keys(el);
+		// SORTING KEYS
+		var keys = [];
+		var sequence = [];
+		try {
+			sequence = sequences[xpath];
+			for (var e of sequence) {
+				if (unorderedKeys.indexOf(e) > -1) {
+					keys.push(e);
+				}
+			}
+			if (unorderedKeys.length != keys.length) { // missing keys in sequence
+				if (debug) console.log('Sequence incomplete for ' + xpath);
+				keys = unorderedKeys; // keep all keys, still unordered
+			}
+		}
+		catch (err) { // no sequence for xpath
+			if (debug) console.log('No sequence found for ' + xpath);
+			keys = unorderedKeys;
+		}
+		// KEYS SORTED
+		for (var k of keys) {
+			// do not process attributes and value
+			if (k != '$' && k != '_') {
+				parse(el[k], k, xpath);
+			}
+		}
+		for (var i=0 ; i < (xpath.match(/\//g) || []).length -1 ; i++) {
+			xml += '\t';
+		}
+	}
+	// closing element
+	xml += '</' + key + '>\n';
+}
+
+
+function printUsage() {
+		console.log('Usage: node json2xml.js inputFile.json outputFile.xml');
 }
